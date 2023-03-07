0d0000b6156b5f44453acf2cb85c41579bb63f9f
Hamza Mubeen <hamza@glowfishlabs.com>
2023-03-07 16:32:17 +0500

    reset.

--- src/constants/environmentConstants.ts
+++
@@ -1,1 +0,0 @@
-export const REACT_APP_BASE_URL = "https://my-json-server.typicode.com";
--- src/interfaces/interfaces.ts
+++
@@ -1,8 +0,0 @@
-export interface iProduct {
-    colour: string;
-    id: number;
-    img: string;
-    name: string;
-    price: number;
-    quantity: number;
-}
